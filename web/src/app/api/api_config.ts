d643412632a78d5b7972b35081be20c656cc28f4
Keita Nonaka <iKonnyaku40@gmail.com>
2020-06-20 05:12:58 +0900

    add interfaces for front-end

--- web/src/app/api/api_config.ts
+++ web/src/app/api/api_config.ts
@@ -3,7 +3,7 @@ import axiosBase from 'axios';
 const urlBase = 'http://localhost:8080/api/v1';
 const headers = {
   'Content-Type': 'application/json',
-  'X-Requested-With': 'XMLHttpRequest',
+  // 'X-Requested-With': 'XMLHttpRequest',
 };
 
 export const axios = axiosBase.create({
