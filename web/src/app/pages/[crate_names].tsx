c5a8f07d2a9f8c2f0057f977047092c013eba507
Keita Nonaka <iKonnyaku40@gmail.com>
2021-05-07 05:02:40 +0900

    remove GetServerSideProps, bug fixes

--- web/src/app/pages/[crate_names].tsx
+++ web/src/app/pages/[crate_names].tsx
@@ -5,8 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useEffect } from 'react';
-import { GetServerSideProps } from 'next';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
@@ -18,24 +17,41 @@ import ExtraInfo from 'components/shared/ExtraInfo';
 import CratesTable from 'components/[crate_names]/CratesTable';
 import DownloadChart from 'components/[crate_names]/DownloadChart';
 
-type Props = {
-  cratesData: CrateResponse[];
-  downloadsData: Downloads[];
-};
-
-const CratesCompare = ({ cratesData, downloadsData }: Props): JSX.Element => {
+const CratesCompare = (): JSX.Element => {
   const router = useRouter();
   const { crate_names } = router.query;
+  const [cratesData, setCratesData] = useState<CrateResponse[]>([]);
+  const [downloadsData, setDownloadsData] = useState<Downloads[]>([]);
 
   useEffect(() => {
-    // fix url param
-    const crateIds = cratesData.map((d) => d.crate.id);
-    if (crateIds.length === 0) {
-      router.push('/');
-    } else if (String(crate_names).split('+').length !== crateIds.length) {
-      router.push('/[crate_names]', `/${crateIds.join('+')}`);
-    }
-  }, [cratesData]);
+    let unmounted = false;
+    const func = async () => {
+      if (!crate_names) return;
+      const crateNames = Array.from(new Set(crate_names.toString().split('+'))); // unique!
+      const crateRequests = crateNames.map((name) => fetchCrateDataUsingGET(name));
+      const downloadRequests = crateNames.map((name) => fetchDownloadDataUsingGET(name));
+      const cratesResults = await Promise.all(crateRequests);
+      const downloadResults = await Promise.all(downloadRequests);
+      const cratesData = cratesResults.filter((v) => v);
+      const downloadsData = downloadResults.filter((v) => v);
+      if (!unmounted) {
+        setCratesData(cratesData);
+        setDownloadsData(downloadsData);
+      }
+
+      const crateIds = cratesData.map((d) => d.crate.id);
+      if (crateIds.length === 0) {
+        router.push('/');
+      } else if (String(crate_names).split('+').length !== crateIds.length) {
+        router.push('/[crate_names]', `/${crateIds.join('+')}`);
+      }
+    };
+    func();
+    const cleanup = () => {
+      unmounted = true;
+    };
+    return cleanup;
+  }, [crate_names]);
 
   return (
     <Wrapper>
@@ -48,17 +64,6 @@ const CratesCompare = ({ cratesData, downloadsData }: Props): JSX.Element => {
       <ExtraInfo />
     </Wrapper>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const crateNames = Array.from(new Set(context.params.crate_names.toString().split('+'))); // unique!
-  const crateRequests = crateNames.map((name) => fetchCrateDataUsingGET(name));
-  const downloadRequests = crateNames.map((name) => fetchDownloadDataUsingGET(name));
-  const cratesResults = await Promise.all(crateRequests);
-  const downloadResults = await Promise.all(downloadRequests);
-  const cratesData = cratesResults.filter((v) => v);
-  const downloadsData = downloadResults.filter((v) => v);
-  return { props: { cratesData, downloadsData } };
 };
 
 const Wrapper = styled.div`
