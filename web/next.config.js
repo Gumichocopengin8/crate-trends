f28ed180058d9f7409cae24b2510456fb517f581
Keita Nonaka <iKonnyaku40@yahoo.co.jp>
2021-05-07 21:52:02 +0900

    change folder structure (#32)
    * change: change folder structure
    
    * fix: minor fixes
    
    * fix: minor fixes

--- web/next.config.js
+++ web/next.config.js
@@ -11,7 +11,7 @@ const path = require('path');
 const isProd = process.env.ENVIRONMENT === 'product_env';
 
 module.exports = {
-  distDir: '../../build/next',
+  distDir: './build',
 
   env: {
     BASE_URL: isProd ? process.env.BASE_URL : 'http://localhost:8080/api/v1',
@@ -23,9 +23,9 @@ module.exports = {
 
     config.plugins = [...config.plugins];
 
-    config.resolve.alias['components'] = path.join(__dirname, 'components');
-    config.resolve.alias['api'] = path.join(__dirname, 'api');
-    config.resolve.alias['interfaces'] = path.join(__dirname, 'interfaces');
+    config.resolve.alias['components'] = path.join(__dirname, 'src/components');
+    config.resolve.alias['api'] = path.join(__dirname, 'src/api');
+    config.resolve.alias['interfaces'] = path.join(__dirname, 'src/interfaces');
     return config;
   },
 };
