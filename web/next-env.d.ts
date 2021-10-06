971232c868748c42c40c948511215949b1a07370
Keita Nonaka <iKonnyaku40@gmail.com>
2021-10-06 11:08:48 -0700

    update: npm modules (#39)

--- web/next-env.d.ts
+++ web/next-env.d.ts
@@ -1,10 +1,6 @@
-/**
- * Copyright (c) 2020-Present, Keita Nonaka
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
