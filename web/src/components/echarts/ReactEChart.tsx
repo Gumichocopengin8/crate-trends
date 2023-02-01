e8e6f1833b3da73b52e548447ce3efa50c60ed25
Keita Nonaka <iKonnyaku40@gmail.com>
2023-01-31 22:27:23 -0800

    chore: switch to npm

--- web/src/components/echarts/ReactEChart.tsx
+++ web/src/components/echarts/ReactEChart.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2020-Present, Keita Nonaka
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import type { EChartsOption, SetOptionOpts } from 'echarts';
 import { BarChart, LineChart } from 'echarts/charts';
 import {
