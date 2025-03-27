0b531319c509e6e6d7fb75875f94832584d7fcc4
Keita Nonaka <iKonnyaku40@gmail.com>
2025-03-27 01:08:48 -0700

    fix: invalid link (#126)

--- web/src/components/shared/table/StatsTableHead.tsx
+++ web/src/components/shared/table/StatsTableHead.tsx
@@ -12,7 +12,6 @@ const StatsTableHead = () => {
         <TableCell align="right">Created At</TableCell>
         <TableCell align="center">Doc</TableCell>
         <TableCell align="center">Homepage</TableCell>
-        <TableCell align="center">Crates.io</TableCell>
         <TableCell align="center">Repo</TableCell>
         <TableCell />
       </TableRow>
